e8defcbf6a1d375cbaa6409ad5a307be078aee6c
orujovcs <orujovcs@gmail.com>
2022-11-03 20:57:02 +0400

    crated: ADD,DELETE,SCROL

--- functionality.js
+++ functionality.js
@@ -3,20 +3,23 @@ const addInfo = document.querySelector(".text-block");
 const sortButton = document.querySelector(".sort-button");
 const deleteButton = document.querySelector(".x-button");
 const body = document.querySelector("body");
+const tasks = document.querySelector(".tasks");
+const item = document.querySelector(".item");
+const myNodeList = [];
 
-deleteButton.addEventListener('click', () => {
-    console.log("Hello!");
+
+addButton.addEventListener('click', () => {
+    let newTask = document.createElement('div');
+    newTask.classList.add('item');
+    newTask.innerHTML = `<div class="p-textBlock"><p class="text-blockTask">${addInfo.value}</p> <button class="x-button" type="button"><img class="deletePic" src="photos\\buttonXgrey.svg" alt=""></button></div>`
+    tasks.appendChild(newTask);
+    myNodeList.push(addInfo.value);
+    addInfo.value = '';
+    console.log(myNodeList);
 });
 
-sortButton.addEventListener('click', _event => {
-    console.log("Hello!");
-});
-
-addInfo.addEventListener('keyup', event => {
-    if(event.key == 'Enter' || event.key == 13){
+tasks.addEventListener('click', (event) => {
+    if(event.target.classList.contains('x-button') || event.target.classList.contains('deletePic')){
+        event.target.parentElement.parentElement.remove();
     }
-});
-
-addButton.addEventListener('click', _event => {
-    console.log("Hello!");
 });
