3b706e68f05abdb84fbe55164f9731a1628ac11f
orujovcs <orujovcs@gmail.com>
2022-11-11 13:51:08 +0400

    version 2.0

--- functionality.js
+++ functionality.js
@@ -33,22 +33,26 @@ sortBtn.addEventListener('click', () => {
     if(image.src == `${urlAddress}photos/lowToHighBlack.svg`){
         image.src = `${urlAddress}photos/highToLowBlack.svg`;             
         myNodeList.sort((a,b) =>{
-            if(a.firstChild.value < b.firstChild.value)            
+            if(a.firstChild.readOnly && b.firstChild.readOnly){
+                if(a.firstChild.value < b.firstChild.value)            
                 return -1;    
             else if(a.firstChild.value > b.firstChild.value)
                 return 1;
             return 0;
+            }
         });
         tasks.replaceChildren(...myNodeList);
     }
     else if(image.src == `${urlAddress}photos/highToLowBlack.svg`){
         image.src = `${urlAddress}photos/lowToHighBlack.svg`;        
         myNodeList.sort((a,b) =>{
-            if(a.firstChild.value > b.firstChild.value)            
+            if(a.firstChild.readOnly && b.firstChild.readOnly){
+                if(a.firstChild.value > b.firstChild.value)            
                 return -1;      
             else if(a.firstChild.value < b.firstChild.value)
                 return 1;
             return 0;
+            }
         });
         tasks.replaceChildren(...myNodeList);
     }
@@ -59,8 +63,8 @@ function createTask(){
     newTask.addEventListener('keyup', makeReadOnlyTask);     
     newTask.innerHTML = `<input class="text-block" type="text" draggable="true" ><button class="x-button" type="button"><img class="deletePic" src="photos\\buttonXgrey.svg" alt=""></button>`
     tasks.appendChild(newTask);
-    const tasksHeight = document.querySelector(".input-block");  
-    tasksHeight.scrollBy(0,110);
+    const tasksHeight = document.querySelector(".input-block"); 
+    tasksHeight.scrollBy(0,110); 
 }
 function makeReadOnlyTask(event){
     if(event.key == "Enter" || event.key == 13){
@@ -69,19 +73,27 @@ function makeReadOnlyTask(event){
 }
 tasks.addEventListener('click', (event) => {
     if(event.target.classList.contains('x-button') || event.target.classList.contains('deletePic')){
-        let htmlCode = document.querySelector(".text-block");
-        if(items.length == 1){
-            htmlCode.value = ' ';
+        if(event.target.parentElement.parentElement.querySelector(".text-block").readOnly == false){
+            event.target.parentElement.parentElement.querySelector(".text-block").value = ' ';
+            event.target.parentElement.parentElement.querySelector(".text-block").readOnly = false;
         }
         else{
-            let deletedElement = event.target.parentElement.parentElement;                
+            let deletedElement = event.target.parentElement.parentElement;     
             deletedElement.remove();
         }   
     }
 });
+document.addEventListener('keyup',() => {
+    const tasksHeight = document.querySelector(".input-block"); 
+    tasksHeight.scrollBy(0,40);
+});
 const taskList = document.getElementById("tasks-list");
-const sortable = Sortable.create(taskList,{
-    animation: 200,
-    ghostClass: 'myghostclass',
-    dragClass: 'sortable-drag'
+taskList.addEventListener('click',(event) => {
+    if(event.target.readOnly == true){
+        const sortable = Sortable.create(taskList,{
+        animation: 200,
+        ghostClass: 'myghostclass',
+        dragClass: 'sortable-drag'
+    });
+    }
 });
